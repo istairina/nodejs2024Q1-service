988419d73f4dd317984186dc0f718ed794729f33
Irina Migunova <ista.irina@gmail.com>
2023-07-30 02:06:17 +0300

    feat: add module to use env values

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,18 @@ import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { FavouriteModule } from './favourite/favourite.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, DatabaseModule, TrackModule, ArtistModule, AlbumModule, FavouriteModule],
+  imports: [
+    UserModule,
+    DatabaseModule,
+    TrackModule,
+    ArtistModule,
+    AlbumModule,
+    FavouriteModule,
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
