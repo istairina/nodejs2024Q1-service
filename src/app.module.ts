59af9671e4b69f1bf58bd782be16ed4cc965dab5
Irina Migunova <ista.irina@gmail.com>
2024-03-26 00:38:36 +0300

    feat: add database_+url to env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './users/user.module';
-import { DatabaseModule } from './database/database.module';
 import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { FavouriteModule } from './favourite/favourite.module';
 import { ConfigModule } from '@nestjs/config';
-import { SignupModule } from './signup/signup.module';
-import { AuthModule } from './auth/auth.module';
-import { SignupModule } from './signup/signup.module';
+
 @Module({
   imports: [
     UserModule,
-    DatabaseModule,
     TrackModule,
     ArtistModule,
     AlbumModule,
     FavouriteModule,
     ConfigModule.forRoot({ isGlobal: true }),
-    DatabaseModule,
-    SignupModule,
-    AuthModule,
   ],
 })
 export class AppModule {}
